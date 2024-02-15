bf1ae6ec41476ec60de01dd624e4548763fc4df6
kyawlinnthant <kyawlinnthant@ilbcedu.com>
2024-02-15 12:55:41 +0630

    'update'

--- 03-hibernates-jpa/01-cruddemo/src/main/java/com/kltdev/cruddemo/dao/StudentDAOImp.java
+++ 03-hibernates-jpa/01-cruddemo/src/main/java/com/kltdev/cruddemo/dao/StudentDAOImp.java
@@ -2,9 +2,12 @@ package com.kltdev.cruddemo.dao;
 
 import com.kltdev.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImp implements StudentDAO {
@@ -22,5 +25,53 @@ public class StudentDAOImp implements StudentDAO {
     @Transactional
     public void save(Student theStudent) {
         entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName desc", Student.class);
+
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+
+        // set query parameter
+        theQuery.setParameter("theData", lastName);
+
+        // return query results
+
+
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        Student theStudent = entityManager.find(Student.class, id);
+
+        entityManager.remove(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        return entityManager.createQuery("DELETE FROM Student").executeUpdate();
     }
 }
